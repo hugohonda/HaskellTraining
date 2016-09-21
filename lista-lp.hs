2aeaca7681280978d3f068157c275c80e4b9ac37
Hugo Honda <hugohondaf@gmail.com>
2016-09-21 19:53:27 -0300

    substitution tab-space

--- lista-lp.hs
+++ lista-lp.hs
@@ -2,27 +2,23 @@
 -- Parte 1 - 2 questões
 
 -- 4. Utilize recursão para criar uma função que calcule 2n - Hugo
-doisElevadoA x  | x==0 = 1
-                | otherwise = 2*doisElevadoA(x-1)
+doisElevadoA x  | x==0 = 1 | otherwise = 2*doisElevadoA(x-1)
 
 -- 5. Escreva uma função que calcule o dobro da soma dos elementos de uma lista - Hugo
-dobroDaSomaLista ls | (length ls) == 0 = 0
-                    | otherwise = 2*(foldl1(+)ls)
+dobroDaSomaLista ls | (length ls) == 0 = 0 | otherwise = 2*(foldl1(+)ls)
 
 -------------------------------------------------------------
 -- Parte 2 - 3 questões
 
 -- 10. Escreva uma função que retorne o número de vezes que um dado elemento aparece numa lista - Hugo
 vezesNaLista x [] = 0
-vezesNaLista x (l:ls)   | x==l = 1 + vezesNaLista x ls
-                        | otherwise = vezesNaLista x ls
+vezesNaLista x (l:ls)   | x==l = 1 + vezesNaLista x ls | otherwise = vezesNaLista x ls
 
 --11. Escreva uma função que receba uma lista e retorne o número de elementos que estão acima da média dos valores da lista - Éden
 acimaMedia (x:xs) = length [y | y <- (x:xs), y > sum (x:xs) `div` length (x:xs)]
 
 -- 14. Escreva uma função que receba uma lista de números e retorne uma tupla com uma lista dos números pares e uma lista dos números ímpares - Hugo
-parEImpar ls    | (length ls)<1 = []
-                | otherwise = zip [filter even ls] [filter odd ls]
+parEImpar ls | (length ls)<1 = [] | otherwise = zip [filter even ls] [filter odd ls]
 
 -------------------------------------------------------------
 -- Parte 3 - 3 questões
@@ -30,23 +26,24 @@ parEImpar ls    | (length ls)<1 = []
 -- 17. Palindrome é uma seqüência de caracteres igual se lida da esquerda para a direita ou ao contrário. As seqüências - Hugo
 -- %&*+--+*&%, abcdefedcba, 0110 e 0112110 são palíndromes. Escreva uma função que verifique se uma string é
 -- uma palindrome. A análise deve ser caracter a caracter, sem reversão da string e verificação de igualdade entre elas.
-checaPalindrome (l:ls)  | (length ls) <= 1 = True
-                        | l==(last ls) = checaPalindrome (drop ((length ls) - 1) ls)
-                        | otherwise = False
+checaPalindrome (l:ls)  | (length ls) <= 1 = True | l==(last ls) = checaPalindrome (drop ((length ls) - 1) ls) | otherwise = False
+
+-- 18. Escreva uma função que quebre uma string em duas partes no ponto onde estiver uma “/” e retorne uma tupla com as duas partes.
+quebraEmBarra [] = []
+quebraEmBarra (l:ls) | l/='/' = [l:(quebraEmBarra ls)] | otherwise = []
+
+-- 20. Estenda a função map f xs (retorna a lista dos valores de f aplicada à lista xs) para map3 f xs ys zs (retorna a lista dos resultados de f aplicada às triplas (x,y,z) onde x   xs, y   ys e z   zs).
+-- map3 f xs ys zs =[f x y z | (x,y,z) <- zip3 xs ys zs]
 
 -------------------------------------------------------------
 -- Parte 4 - Todas
 
 -- 22. Crie uma nova função que utilize a expressão where - Hugo
-somaDoisBits x y    | x/=0&&x/=1||y/=0&&y/=1 = "Parametros devem ser apenas 0 ou 1"
-somaDoisBits x y    | soma < 1 = "0"
-                    | soma == 1 = "1"
-                    | soma > 1 = "overflow"
-                    where soma = x + y
+somaDoisBits x y | x/=0&&x/=1||y/=0&&y/=1 = "Parametros devem ser apenas 0 ou 1"
+somaDoisBits x y | soma < 1 = "0" | soma == 1 = "1" | soma > 1 = "overflow" where soma = x + y
 
 -- 23. Crie uma nova função que utilize a expressão let - Hugo
-quantosAnosTem ano =    let anoAtual=2016
-                        in  anoAtual-ano
+quantosAnosTem ano = let anoAtual=2016 in anoAtual-ano
 
 -- 24. Crie uma nova função que utilize uma função lambda e a função map - Hugo
 potenciaLista = (\ls x -> map (^x) ls)
@@ -63,6 +60,6 @@ potenciaLista = (\ls x -> map (^x) ls)
 --
 
 teste = do
-        putStrLn "O animal é um mamífero?"
-        name <- getLine
-        putStrLn ("Então, " ++ name ++ "")
+ putStrLn "O animal é um mamífero?"
+ name <- getLine
+ putStrLn ("Então, " ++ name ++ "")
