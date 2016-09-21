bde5b1ebb7a427f73e06a5e258bf25559df03603
Hugo Honda <hugohondaf@gmail.com>
2016-09-21 11:38:03 -0300

    updated authors

--- haskell_tut.hs
+++
@@ -1,5 +0,0 @@
-import Data.List
-import System.IO
-
-factorial :: Integer -> Integer
-factorial n = product [1..n]
--- lista-lp.hs
+++ lista-lp.hs
@@ -1,33 +1,33 @@
 -------------------------------------------------------------
 -- Parte 1 - 2 questões
 
--- 4. Utilize recursão para criar uma função que calcule 2n
+-- 4. Utilize recursão para criar uma função que calcule 2n - Hugo
 doisElevadoA x  | x==0 = 1
                 | otherwise = 2*doisElevadoA(x-1)
 
--- 5. Escreva uma função que calcule o dobro da soma dos elementos de uma lista.
+-- 5. Escreva uma função que calcule o dobro da soma dos elementos de uma lista - Hugo
 dobroDaSomaLista ls | (length ls) == 0 = 0
                     | otherwise = 2*(foldl1(+)ls)
 
 -------------------------------------------------------------
 -- Parte 2 - 3 questões
 
--- 10. Escreva uma função que retorne o número de vezes que um dado elemento aparece numa lista.
+-- 10. Escreva uma função que retorne o número de vezes que um dado elemento aparece numa lista - Hugo
 vezesNaLista x [] = 0
 vezesNaLista x (l:ls)   | x==l = 1 + vezesNaLista x ls
                         | otherwise = vezesNaLista x ls
 
---11. Escreva uma função que receba uma lista e retorne o número de elementos que estão acima da média dos valores da lista.
+--11. Escreva uma função que receba uma lista e retorne o número de elementos que estão acima da média dos valores da lista - Éden
 acimaMedia (x:xs) = length [y | y <- (x:xs), y > sum (x:xs) `div` length (x:xs)]
 
--- 14. Escreva uma função que receba uma lista de números e retorne uma tupla com uma lista dos números pares e uma lista dos números ímpares.
+-- 14. Escreva uma função que receba uma lista de números e retorne uma tupla com uma lista dos números pares e uma lista dos números ímpares - Hugo
 parEImpar ls    | (length ls)<1 = []
                 | otherwise = zip [filter even ls] [filter odd ls]
 
 -------------------------------------------------------------
 -- Parte 3 - 3 questões
 
--- 17. Palindrome é uma seqüência de caracteres igual se lida da esquerda para a direita ou ao contrário. As seqüências
+-- 17. Palindrome é uma seqüência de caracteres igual se lida da esquerda para a direita ou ao contrário. As seqüências - Hugo
 -- %&*+--+*&%, abcdefedcba, 0110 e 0112110 são palíndromes. Escreva uma função que verifique se uma string é
 -- uma palindrome. A análise deve ser caracter a caracter, sem reversão da string e verificação de igualdade entre elas.
 checaPalindrome (l:ls)  | (length ls) <= 1 = True
@@ -37,18 +37,18 @@ checaPalindrome (l:ls)  | (length ls) <= 1 = True
 -------------------------------------------------------------
 -- Parte 4 - Todas
 
--- 22. Crie uma nova função que utilize a expressão where.
+-- 22. Crie uma nova função que utilize a expressão where - Hugo
 somaDoisBits x y    | x/=0&&x/=1||y/=0&&y/=1 = "Parametros devem ser apenas 0 ou 1"
 somaDoisBits x y    | soma < 1 = "0"
                     | soma == 1 = "1"
                     | soma > 1 = "overflow"
                     where soma = x + y
 
--- 23. Crie uma nova função que utilize a expressão let.
+-- 23. Crie uma nova função que utilize a expressão let - Hugo
 quantosAnosTem ano =    let anoAtual=2016
                         in  anoAtual-ano
 
--- 24. Crie uma nova função que utilize uma função lambda e a função map.
+-- 24. Crie uma nova função que utilize uma função lambda e a função map - Hugo
 potenciaLista = (\ls x -> map (^x) ls)
 
 -- 25. Crie uma função que seja polimórfica e que use o operador de composição de funções.
@@ -59,6 +59,8 @@ potenciaLista = (\ls x -> map (^x) ls)
 
 -------------------------------------------------------------
 -- Parte 5
+
+--
 
 teste = do
         putStrLn "O animal é um mamífero?"
